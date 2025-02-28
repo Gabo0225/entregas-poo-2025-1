94d650d79c97b9f3e1d3acd523a81fae0f91f259
gabo0225 <gabrielsaher@hotmail.com>
2025-02-27 21:53:30 -0500

    Taller

--- TIenda 1/Inventaro.py
+++ TIenda 1/Inventaro.py
@@ -1,29 +1,51 @@
 #!/usr/bin/env python3
 
 """
-Título de práctica: breve descripción
+Título de práctica: Inventario tienda 1
 
 Descripción extendida del programa
 
-Autor: ElAutor <el@correo>
-Fecha: 2025-02-01
+Autor: Gabriel Santamaria <gabrielsaher@hotmail.com>
+Fecha: 2025-02-27
 """
 
-# **** En esta región puede importar los módulos necesarios para su programa
-# **** O las definiciones de clases y/o funciones que requiera.
+class inventario: 
+    """Clase que representa el inventario de una tienda"""
+    def __init__(self, nombre, cantidad, precio):
+        self.nombre = nombre
+        self.cantidad = cantidad
+        self.precio = precio
+    
+    #muestra los productos seleccionados
+    def show_inventory(self):
+        return f"{self.nombre:<12}" f"{self.cantidad:<8}" f"{self.precio:<8}"
 
+def add_product():
+    """Agrega un producto al inventario"""
+    nombre = input("Nombre del producto: ")
+    cantidad = int(input(f"Cantidad del producto {nombre}: "))
+    precio = int(input(f"Precio del unitario del producto {nombre}: "))
+    return inventario(nombre, cantidad, precio)
 
-def run():
-    """script entrypoint"""
+def see_inventory(inventario):
+    """Muestra el inventario"""
+    print ("\n Nombre Cantidad Precio")
+    print("-----------------------")
+    #Muestra el inventario
+    for producto in inventario:
+        print(producto.show_inventory())
 
-    # **** Poner el código ejecutable de su ejercicio aquí
-
-    # Saludo
-    print("Hola mundo!")
-
-    # **** ****
-
-
-# **** Conserve este condicional para ejecutar el programa directamente
+def main():
+    productos = []
+    # Pedimos los datos para añadir los productos
+    for i in range(3):
+        print(f"\n {i+1} Producto:")
+        producto = add_product()
+        productos.append(producto)
+    #muestra el inventario en forma de tabla
+    print("\nDatos de los productos ingresados:")
+    see_inventory(productos)
+         
 if __name__ == "__main__":
-    run()
+    main()
+        run()
